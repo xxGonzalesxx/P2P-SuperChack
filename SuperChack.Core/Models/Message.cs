438481514b03f4d74af28aab5a4457560c92f10d
Stefan <stef.kir1999@gmail.com>
2026-07-09 03:37:03 +0400

    feat(core): добавлена модель Message для данных чата

---
+++ SuperChack.Core/Models/Message.cs
@@ -0,0 +1,9 @@
+﻿namespace SuperChack.Core.Models;
+
+public class Message
+{
+    public int Id { get; set; }
+    public string Sender { get; set; } = string.Empty;
+    public string Text { get; set; } = string.Empty;
+    public DateTime SentAt { get; set; }
+}
