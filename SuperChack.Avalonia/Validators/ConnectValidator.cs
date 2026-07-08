219d5bf151a26ea7e5dfec7106e6eb595621679f
Stefan <stef.kir1999@gmail.com>
2026-07-09 03:37:42 +0400

    feat(avalonia): добавлена валидация формы подключения через FluentValidation

---
+++ SuperChack.Avalonia/Validators/ConnectValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using SuperChack.Avalonia.ViewModels;
+
+namespace SuperChack.Avalonia.Validators;
+
+public class ConnectValidator : AbstractValidator<ConnectViewModel>
+{
+    public ConnectValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Введи имя.");
+
+        RuleFor(x => x.PeerIp)
+            .NotEmpty().WithMessage("Введи IP.")
+            .Matches(@"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
+            .WithMessage("Неверный формат IP. Пример: 26.131.93.127");
+    }
+}
